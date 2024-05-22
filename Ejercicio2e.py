12ad3ecc2fb308cf7add3797bfd04a667c07a1ea
FernandoGarambelM <fgarambel@unsa.edu.pe>
2024-05-22 09:39:16 -0500

    Ejercicio 2e resuelto

---
+++ Ejercicio2e.py
@@ -0,0 +1,7 @@
+from interpreter import draw
+from chessPictures import *
+square = Picture(SQUARE) 
+blackSquare = square.negative()
+bloque = square.join(blackSquare)
+fila = bloque.horizontalRepeat(4)
+draw(fila)

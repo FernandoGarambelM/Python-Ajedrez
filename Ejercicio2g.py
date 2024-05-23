12484e3d440b07a18de6cdc08668541e6bb298e1
FernandoGarambelM <fgarambel@unsa.edu.pe>
2024-05-22 22:47:10 -0500

    Ejercicio2g completo, tablero de ajedrez

--- Ejercicio2g.py
+++ Ejercicio2g.py
@@ -1,6 +1,40 @@
 from interpreter import draw
 from chessPictures import *
-square = Picture(SQUARE) 
+
+bishop = Picture(BISHOP)
+blackBishop = bishop.negative()
+king = Picture(KING)
+blackKing = king.negative()
 knight = Picture(KNIGHT)
-tablero = square.under(knight)
+blackKnight = knight.negative()
+pawn = Picture(PAWN)
+blackPawn = pawn.negative()
+queen = Picture(QUEEN)
+blackQueen = queen.negative()
+rock = Picture(ROCK)
+blackRock = rock.negative()
+square = Picture(SQUARE)
+blackSquare = square.negative()
+
+bloque = square.join(blackSquare)
+fila = bloque.horizontalRepeat(4)
+filaNegativa = fila.negative()
+
+filaPiezasBlancas = rock.join(knight).join(bishop).join(queen).join(king).join(bishop).join(knight).join(rock)
+filaPeones = pawn.horizontalRepeat(8)
+filaPeonesNegros = filaPeones.negative()
+filaPiezasNegras = filaPiezasBlancas.negative()
+
+primeraFila = fila.under(filaPiezasNegras)
+segundaFila = filaNegativa.under(filaPeonesNegros)
+tableroNegras = primeraFila.up(segundaFila)
+
+tableroSinFichas = fila.up(filaNegativa).verticalRepeat(2)
+
+penultimaFila = fila.under(filaPeones)
+ultimaFila = filaNegativa.under(filaPiezasBlancas)
+tableroBlancas = penultimaFila.up(ultimaFila)
+
+tablero = tableroNegras.up(tableroSinFichas.up(tableroBlancas))
+
 draw(tablero)
